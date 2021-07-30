db4db1559fcfd0460c665a7fa42e16f1650ce3f3
Luca <lucaciprani1@gmail.com>
2021-07-30 17:02:59 +0200

    fomatted

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 // Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
 
-var students = [{ 
-    'name' : 'Luca',
-    'surname' : 'Ciprani',
-    'age' : 28,
+var students = [{
+    'name': 'Luca',
+    'surname': 'Ciprani',
+    'age': 28,
 }]
 
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 
-for(let i = 0; i < students.length; i++){
-    
-    for(let key in students[i]) {
+for (let i = 0; i < students.length; i++) {
+
+    for (let key in students[i]) {
         var studentProperties = students[i][key];
         console.log(key + ' : ' + studentProperties);
     }
@@ -20,49 +20,58 @@ for(let i = 0; i < students.length; i++){
 // // Creare un array di oggetti di studenti.
 
 let firstObject = {
-    'name' : 'Marco',
-    'surname' : 'Rossi',
-    'age' : 26,
+    'name': 'Marco',
+    'surname': 'Rossi',
+    'age': 26,
 };
 
 students.push(firstObject);
-    
+
 let secondObject = {
-    'name' : 'Pietro',
-    'surname' : 'gialli',
-    'age' : 23,
+    'name': 'Pietro',
+    'surname': 'Gialli',
+    'age': 23,
 };
 
 students.push(secondObject);
-    
-// console.log(students);
-    
-// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
 
-for(let i = 0; i < students.length; i++){
-    
-    for(let key in students[i]) {
-        var studentProperties = students[i][key];
-        console.log(key + ' : ' + studentProperties);
+//  Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+
+for (let i = 0; i < students.length; i++) {
+
+    for (let key in students[i]) {
+        console.log(students[i]['name'] + ' ' + students[i]['surname']);
     }
-    console.log(students[i]);
 }
 
 // Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
-setTimeout(function(){ 
-    
+setTimeout(function () {
+
     let enterName = prompt("inserisci il tuo nome");
     let enterSurname = prompt("inserisci il tuo cognome");
     let enterAge = parseInt(prompt("inserisci la tua età"));
 
     let thirdObject = {
-        'name' : enterName,
-        'surname' : enterSurname,
-        'age' : enterAge
+        'name': enterName,
+        'surname': enterSurname,
+        'age': enterAge
     };
 
     students.push(thirdObject);
     console.log(students);
+
+
+    // stampo a schermo il totale degli studentProperties, con aggiunti;
+
+    for (let i = 0; i < students.length; i++) {
+
+        for (let key in students[i]) {
+            var studentProperties = students[i][key];
+            console.log(key + ' : ' + studentProperties);
+        }
+        console.log(students[i]);
+    }
+
 
 }, 5000);
